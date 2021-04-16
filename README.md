67307bfac3316761c81d96ab14cbe9cf49e11ce5
Filippo <zzkw35@gmail.com>
2021-04-16 02:50:35 +0200

    Allow to use a custom branch name

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 **Usage**:
 
-      bash dumpyara.sh "<OTAlink> OR <OTA file path>" yourGithubToken
+      bash dumpyara.sh "<OTAlink> OR <OTA file path>" yourGithubToken customBranchName
 
 You can also place your github oauth token in a file called `.githubtoken` and telegram bot token in a file called `.tgtoken` in the root of this repository, if you wish it is ignored by git.  
 Before you start, make sure that dumpyara scripts are mapped to your own org or account, otherwise you'll only dump, not push.  
--- dumpyara.sh
+++ dumpyara.sh
@@ -156,7 +156,11 @@ description=$(grep -oP "(?<=^ro.build.description=).*" -hs {system,system/system
 [[ -z "${description}" ]] && description="$flavor $release $id $incremental $tags"
 is_ab=$(grep -oP "(?<=^ro.build.ab_update=).*" -hs {system,system/system,vendor}/build*.prop | head -1)
 [[ -z "${is_ab}" ]] && is_ab="false"
-branch=$(echo "$description" | tr ' ' '-')
+if [[ -n $3 ]]; then #Check whether or not the user has entered a custom branch name
+    branch=$3
+else
+    branch=$(echo "$description" | tr ' ' '-')
+fi
 repo=$(echo "$brand"_"$codename"_dump | tr '[:upper:]' '[:lower:]')
 platform=$(echo "$platform" | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]' | tr '_' '-' | cut -c 1-35)
 top_codename=$(echo "$codename" | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]' | tr '_' '-' | cut -c 1-35)
